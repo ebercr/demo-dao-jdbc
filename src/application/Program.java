a899f161e2fbea348da1f0a0d9184f2f1e1f22b6
Eber Rabello <ebercr@gmail.com>
2024-06-08 21:56:03 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,18 +5,13 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.time.LocalDate;
-
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== Teste 1: seller findById =======");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
-        System.out.println(obj);
     }
 }
